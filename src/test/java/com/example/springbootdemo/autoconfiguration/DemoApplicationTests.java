be9dd264a29585df9d056c691e5b9f12133811b3
Maksim <madmaxim22@gmail.com>
2023-04-14 22:46:28 +0300

    refactor: integration test containers docker whit annotation

--- src/test/java/com/example/springbootdemo/autoconfiguration/DemoApplicationTests.java
+++ src/test/java/com/example/springbootdemo/autoconfiguration/DemoApplicationTests.java
@@ -7,23 +7,24 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Testcontainers
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DemoApplicationTests {
-    private static final GenericContainer<?> myAppFirst = new GenericContainer<>("devapp:latest")
-            .withExposedPorts(8080);
-    private static final GenericContainer<?> myAppSecond = new GenericContainer<>("prodapp:latest")
-            .withExposedPorts(8081);
+    
     @Autowired
     TestRestTemplate restTemplate;
     
-    @BeforeAll
-    public static void setUp() {
-        myAppFirst.start();
-        myAppSecond.start();
-    }
+    @Container
+    private static final GenericContainer<?> myAppFirst = new GenericContainer<>("devapp:latest")
+            .withExposedPorts(8080);
+    @Container
+    private static final GenericContainer<?> myAppSecond = new GenericContainer<>("prodapp:latest")
+            .withExposedPorts(8081);
     
     @Test
     void contextLoadDevApp() {
